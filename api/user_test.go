dc34f4feea8f8e028d0704a9ea4eeb714e459a5a
kons77 <staneua@gmail.com>
2025-05-16 21:03:29 +0300

    add token insertion logic to api/server.go and update tests

--- api/user_test.go
+++ api/user_test.go
@@ -181,7 +181,7 @@ func TestCreateUserAPI(t *testing.T) {
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStubs(store)
 
-			server := NewServer(store) // newTestServer(t, store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			// Marshal body data to JSON
