549c433e8def7bc8d8376a2907ebf0d4d9de7edc
kons77 <staneua@gmail.com>
2025-06-15 19:38:48 +0300

    add support for running gRPC server and HTTP gateway in main.go

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/kons77/simplebank/api"
 	db "github.com/kons77/simplebank/db/sqlc"
 	"github.com/kons77/simplebank/gapi"
@@ -12,6 +14,7 @@ import (
 	"github.com/kons77/simplebank/util"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -31,6 +34,7 @@ func main() {
 	}
 
 	store := db.NewStore(connPool)
+	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
 
 }
@@ -55,6 +59,47 @@ func runGrpcServer(config util.Config, store *db.SQLStore) {
 	err = grpcServer.Serve(listener)
 	if err != nil {
 		log.Fatal("cannot start gRPC server:", err)
+	}
+}
+
+// start grpc gateway server
+func runGatewayServer(config util.Config, store *db.SQLStore) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	// to generate JSON with snake_case iunstead of camelCase
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot register handler server:", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener:", err)
+	}
+
+	log.Printf("start HTTP gateway server at %s", listener.Addr().String())
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("cannot start HTTP gateway server:", err)
 	}
 }
 
